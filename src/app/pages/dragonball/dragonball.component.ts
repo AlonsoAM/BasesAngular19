9366786655bfdab32cad6e41df9ec6392ca5a0ff
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-02-28 14:24:22 -0500

    Mejora el componente Dragonball para mostrar personajes con poderes superiores a 1000 y actualiza la lógica de estilos según el nivel de poder.

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 
 interface Character {
   id: number;
@@ -14,8 +19,19 @@ interface Character {
 })
 export class DragonballComponent {
   characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9000 },
+    { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
     { id: 3, name: 'Gohan', power: 7000 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ]);
+
+  // powerClasses = computed(() => {
+  //   return this.characters().map((character) => {
+  //     return {
+  //       'text-success': character.power > 8000,
+  //       'text-warning': character.power <= 8000 && character.power > 7000,
+  //       'text-danger': character.power <= 7000,
+  //     };
+  //   });
+  // });
 }
