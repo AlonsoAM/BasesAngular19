fa13c7938d2db92349018d0a1a1f8160e7427d7f
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-02-28 13:56:04 -0500

    Añade componente Dragonball y actualiza rutas y navbar para incluirlo.

---
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball',
+  imports: [],
+  templateUrl: './dragonball.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DragonballComponent {}
