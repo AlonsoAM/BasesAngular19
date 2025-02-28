58d4d6c64b0d8009e23bef499026293a9601d08b
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-02-28 14:40:21 -0500

    Añade funcionalidad para agregar personajes en el componente Dragonball, incluyendo campos de entrada para nombre y poder, y lógica para manejar la adición de nuevos personajes.

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -18,12 +18,36 @@ interface Character {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DragonballComponent {
+  name = signal<string>('');
+  power = signal<number>(0);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
     { id: 3, name: 'Gohan', power: 7000 },
     { id: 4, name: 'Yamcha', power: 500 },
   ]);
+
+  addCharacter() {
+    if (!this.name() || this.power() === 0 || this.power() < 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((chars) => [...chars, newCharacter]);
+
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 
   // powerClasses = computed(() => {
   //   return this.characters().map((character) => {
