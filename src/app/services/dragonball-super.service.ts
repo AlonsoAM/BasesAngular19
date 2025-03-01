e0b189b251076258eafbd63e15bbde4a6f056d8f
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-02-28 19:20:05 -0500

    Añade funcionalidad para cargar y guardar personajes en localStorage en el servicio Dragonball Super.

--- src/app/services/dragonball-super.service.ts
+++ src/app/services/dragonball-super.service.ts
@@ -1,11 +1,20 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+function loadFromLocalStorage() {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class DragonballSuperService {
-  characters = signal<Character[]>([]);
+  characters = signal<Character[]>(loadFromLocalStorage());
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
   addCharacter(character: Character) {
     this.characters.update((chars) => [...chars, character]);
